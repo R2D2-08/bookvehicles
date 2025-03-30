065fd8d4579b25e8edb86ffcdc6e436c88f9c314
1-Nishanth-1 <nishanthrn1@gmail.com>
2025-03-30 18:31:26 +0530

    deployment suff

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,22 +1,33 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
-
+const fs = require("fs");
+const path = require("path");
 const sequelize = new Sequelize(
-    process.env.DATABASE_NAME, 
-    process.env.DATABASE_USERNAME,
-    process.env.DATABASE_PASSWORD, {
-        host: process.env.DATABASE_HOST,
-        dialect: "mysql"
-    }
-); 
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    port: 22218,
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql",
+    dialectOptions: {
+      connectTimeout: 60000,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: console.log,
+  }
+);
 
-const testConnection = async() => {
-    try {
-        await sequelize.authenticate();
-        console.log("Connected to MYSQL");
-    } catch(error) {
-        console.error(`Unable to connect to MYSQL - ${error}`);
-    }
-}
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connected to MYSQL");
+  } catch (error) {
+    console.error(`Unable to connect to MYSQL - ${error}`);
+  }
+};
 
-module.exports = {testConnection, sequelize};
+module.exports = { testConnection, sequelize };
